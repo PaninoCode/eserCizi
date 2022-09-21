f70fbc1cace0bc8b82c521e7601d6e2aaf47023c
Alex Niccolò Ferrari <41290578+alex65ferrari@users.noreply.github.com>
2022-09-21 09:00:26 +0200

    .

--- esercizio3/esercizio3.c
+++ esercizio3/esercizio3.c
@@ -1,5 +1,19 @@
-/*
-Scrivere un programma in C che dopo aver letto “N” numeri, con N scelto dall’utente, il massimo ed il
-minimo numeri inseriti. Suggerimenti: Controllo di N con N&gt;0, nessun controllo sui Numeri inseriti.
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+int main(int argc, char *argv)
+{
+    char **stringArray[500];
+    printf("Inserisci gli elementi (solo numeri) uno alla volta, inserire il carattere [x] per terminare l'inserimento.");
+    int contatore = 0;
+    char elemento[4];
+    do {
+        printf("\n(%d)> ", contatore);
+        scanf("%s", elemento);
+        if(strcmp(elemento, "x") == 0 && atoi(elemento) == 0 || 1 || 2 || 3 || 4 || 5 || 6 || 7 || 8 || 9){
+            printf("we");
+        } 
+    }while(elemento != "x");
+    return 0;
+}
---
+++ esercizio3/readme_esercizio3.md
@@ -0,0 +1,15 @@
+## Esercizio 3
+
+Scrivere un programma in C che gestica, attraverso un menù, le seguenti operazione sugli array:
+
+1) Stampa degli elementi array;
+2) Stampa inversa degli elementi dell’array;
+3) Somma e media degli elementi del vettore;
+4) Stampa tutti i numeri pari;
+5) Stampa tutti i numeri dispari;
+6) Ricerca di un numero nell’array (letto in input);
+7) Eliminare un elemento dell’array (se esistente);
+8) Alternare (o scambiare ) a due a due le posizioni del vettore:
+    - esempio vettore: 1,2,3,4
+    - vettore alternato: 2,1,4,3 *(attenzione se lungo dispari)*
+9) Ordinamento del vettore (ordinamento a scelta).
